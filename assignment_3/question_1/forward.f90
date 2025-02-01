5eab2bd26577f97013f7d73799f2f4cc895dc656
Akif4362 <aknasif43@gmail.com>
2025-02-01 17:55:59 +0600

    added quesion 1 and 3, fixed question 2

--- assignment_3/question_1/forward.f90
+++ assignment_3/question_1/forward.f90
@@ -1,13 +1,13 @@
-program dividedDifference
+program forward
 implicit none
 integer:: nval= 0, status, i, j, k
-real :: temp, temp2, term, result = 0, input
+real :: temp, temp2, term, result = 0, input, p, h
 real, allocatable, dimension (:) :: x, y
 real, allocatable, dimension (:, :) :: dd
 
 open(unit=42, file="data.txt", action="read")
 open(unit=43, file="input.txt", action="read")
-open(unit=44, file="output_dd.txt", action="write")
+open(unit=44, file="output.txt", action="write")
 
 do
   read(42, *, iostat=status) temp
@@ -35,13 +35,13 @@ end do
 
 do j = 2, nval
   do i = 1, nval - j + 1
-    dd(i, j) = (dd(i+1, j-1) - dd(i, j-1)) / (x(i+j-1) - x(i))
+    dd(i, j) = dd(i+1, j-1) - dd(i, j-1)
   end do
 end do
 
 
-write(44, '(T9, A, T21, A, T38, A, T55, A, T72, A)') "y", "first dd", "second dd", "third dd", "fourth dd"
-write(44, '(T5, A, T21, A, T38, A, T55, A, T72, A)') "---------", "---------", "---------", "---------", "---------"
+write(44, '(T9, A, T21, A, T38, A, T55, A, T72, A, T89, A)') "y", "first dd", "second dd", "third dd", "fourth dd", "fifth dd"
+write(44, '(T5, A, T21, A, T38, A, T55, A, T72, A, T89, A)') "---------", "---------", "---------", "---------", "---------", "---------"
 
 do i = 1, nval
   write(44, *) (dd(i, j), j = 1, nval - i + 1)
@@ -50,12 +50,14 @@ end do
 do
   read(43, *, iostat=status) input
   if (status /= 0) exit
+  h = x(2) - x(1)
+  p = (input - x(1)) / h
   result = 0
-  
+
   do j = 1, nval
     term = dd(1, j)
     do i = 1, j-1
-      term = term * (input - x(i))
+      term = term * ((p - i + 1)/i)
     end do
     result = result + term
   end do
@@ -64,4 +66,4 @@ do
   write(44, *) "For x = ", input, " f(x) = ", result
 end do
 
-end program dividedDifference
+end program forward
