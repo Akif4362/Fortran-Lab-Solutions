ccb8f27e4a6a5eaae9d0f7973ff01954632e9c49
Akif4362 <aknasif43@gmail.com>
2025-05-17 15:57:44 +0600

    fixed assignment 6 question 4

--- assignment_6/question_4/gram-schmidt.f90
+++ assignment_6/question_4/gram-schmidt.f90
@@ -1,46 +1,41 @@
 program gram_schmidt
 implicit none
-integer :: i, j
-real, dimension(3,3) :: matrix
-real, dimension(3) :: u1, u2, u3
-real, dimension(3) :: v1, v2, v3
-real, dimension(3) :: e1, e2, e3
-real :: proj_scaler
+integer :: i, j, n, m
+real, dimension(:,:), allocatable :: u, v, e
+real :: norm, proj_scaler
 open(unit=42, file="input.txt")
 open(unit=43, file="output.txt")
 
-! take input data
-do i = 1, 3
-read(42, *) (matrix(i, j), j = 1, 3)
+! n = number of vectors, m = dimension of each vector
+read(42, *) n, m
+
+allocate(u(n,m), v(n,m), e(n,m))
+
+do i = 1, n
+  read(42, *) (u(i,j), j=1,m)
 end do
 
-! divide data into u1, u2, u3
-do i = 1, 3
-u1(i) = matrix(1, i)
-u2(i) = matrix(2, i)
-u3(i) = matrix(3, i)
+! step 1: v_1 = u_1
+v(1,:) = u(1,:)
+
+! step 2 to n: v_i = u_i - sum_{j=1}^{i-1} proj_{v_j}(u_i)
+do i = 2, n
+  v(i,:) = u(i,:)
+  do j = 1, i-1
+    v(i,:) = v(i,:) - proj_scaler(u(i,:), v(j,:)) * v(j,:)
+  end do
 end do
 
-! step 1: v1 = u1
-v1 = u1
+! step 3: normalize
+do i = 1, n
+  norm = norm2(v(i,:))
+  e(i,:) = v(i,:) / norm
+end do
 
-! step 2: v2 = u2 - proj_v1(u2)
-v2 = u2 - proj_scaler(u2, v1) * v1
-
-! step 3: v3 = u3 - proj_v1(u3) - proj_v2(u3)
-v3 = u3 - proj_scaler(u3, v1) * v1 - proj_scaler(u3, v2) * v2
-
-! normalize the vectors to get the orthonormal basis
-! norm2 is built-in fortran function
-e1 = v1 / norm2(v1)
-e2 = v2 / norm2(v2)
-e3 = v3 / norm2(v3)
-
-! output
 write(43, *) "Orthonormal basis:"
-write(43, *) "e1 = ", e1
-write(43, *) "e2 = ", e2
-write(43, *) "e3 = ", e3
+do i = 1, n
+  write(43, '(A,I0,A,*(F10.6))') "e", i, " = ", e(i,:)
+end do
 
 end program gram_schmidt
 
